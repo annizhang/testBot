222704f62804d2d8011370c58faad4df3b6cb7fa
Anni Zhang <annizhang@Annis-MacBook-Pro-II.local>
2016-06-08 14:43:41 -0400

    can i print the chunk?

--- index.js
+++ index.js
@@ -214,7 +214,7 @@ app.post('/webhook', function (req, res) {
                    var getListings = https.get(fetchListingUrl, function(res){
                        var body = '';
                        res.on('data', function(chunk){
-                           //console.log(chunk);
+                           console.log(chunk);
                            body += chunk;
                        });
                        res.on('end', function(){
