7d62e90aa9c32be2cfec8c08baf8e91ce3b49032
Anni Zhang <annizhang@Annis-MacBook-Pro-II.local>
2016-06-08 18:53:54 -0400

    it doesn't even go in the function?

--- index.js
+++ index.js
@@ -242,8 +242,8 @@ app.post('/webhook', function (req, res) {
                    }).on('error', function(e){
                        console.log("Got an error: ", e);
                    });
-                   sendMessage(event.sender.id, {"text": "Thanks! Here are 5 apartments I think you will be interested in:"});
-                   searchListings(beds, minPrice, maxPrice, event.sender.id,listings);
+                   //sendMessage(event.sender.id, {"text": "Thanks! Here are 5 apartments I think you will be interested in:"});
+                   searchListings(beds, minPrice, maxPrice, event.sender.id, listings);
                } else {
                    sendMessage(event.sender.id, {"text": "hahah what? type 'joinery' to get started finding some no fee apartments or to list your apartment :\)"});
                }
