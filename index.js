941bfa4e60a6d3d34bbce76f2319d31d9a175fa0
Anni Zhang <annizhang@Annis-MacBook-Pro-II.local>
2016-06-13 18:19:58 -0400

    node-redis

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var request = require('request');
 var app = express();
 var http = require('http');
 var https = require('https');
+var redis = require('redis');
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
@@ -255,6 +256,11 @@ function joineryGreeting(recipientId, message) {
 
 // handler receiving messages
 app.post('/webhook', function (req, res) {
+    //connecting to the server
+    var client = redis.createClient(process.env.PORT, 3000);
+    client.on('connect', function() {
+        console.log("connected");
+    });
     //need to create conversation thread
     //create a list of 
     var events = req.body.entry[0].messaging;
