5ef475f9de8b08577e49aee8a9d2c17b28ae9c96
Anni Zhang <annizhang@Annis-MacBook-Pro-II.local>
2016-06-14 15:08:18 -0400

    property connecting to redis url

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var https = require('https');
 var redis = require('redis');
 
 //connecting to the server
-var client = redis.createClient();
+var client = redis.createClient(process.env.REDIS_URL);
 
 client.on('error', function(err){
     console.log('Error ' + err);
