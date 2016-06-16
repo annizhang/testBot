a8fe9fa689a957123102971ad32ea0fbb59b350a
Anni Zhang <annizhang@Annis-MacBook-Pro-II.local>
2016-06-16 17:08:42 -0400

    using criteriaFound properly

--- index.js
+++ index.js
@@ -340,9 +340,9 @@ app.post('/webhook', function (req, res) {
                    if (minPrice === Number.MIN_VALUE || maxPrice === Number.MAX_VALUE){
                        message = {"text":"Hm...I didn't get that, can you please input your price range in the form of 'low to high'?"};
                        sendMessage(event.sender.id, message);
-                   }
-                   criteriaFound = true;
-               } else if(!criteriaFound){
+                       criteriaFound = false;
+                   } else {criteriaFound = true;}
+               } else if(criteriaFound){
                    var getListings = https.get(fetchListingUrl, function(res){
                        var body = '';
                        res.on('data', function(chunk){
