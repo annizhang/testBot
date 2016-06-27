1977747e0bc1d0eb7937e64e38c92588481d4fe7
Anni Zhang <annizhang@Annis-MacBook-Pro-II.local>
2016-06-27 18:34:29 -0400

    fix findlocation problem

--- index.js
+++ index.js
@@ -219,6 +219,20 @@ function locationExists(text,locations) {
             return;
         }
     }
+    if (result[0] === "none") {
+        sendMessage(event.sender.id, {"text": "That's not a place I recognize. Please give me a NYC neighborhood."});
+    } else {
+       //console.log("HERE!");
+       //console.log ("location = " + location[1]);
+       //place = location[1];
+       locationFound = true;
+       console.log("place: " + place);
+       if (apartmentType === "Entire Apartment"){
+       sendMessage(event.sender.id, {"text": "Great! How many bedrooms are you looking for in " + place + "? Please enter a number."});
+       }else {
+           sendMessage(event.sender.id,{"text": "Nice! What is your price range? For example, '1500 to 3000'"});
+       }
+   }
     //return result;
 }
 
@@ -438,7 +452,7 @@ app.post('/webhook', function (req, res) {
                if (!locationFound) {
                    //console.log("looking at location");
                    findLocation(event.message.text, locationExists);
-                   if (result[0] === "none") {
+                   /*if (result[0] === "none") {
                        sendMessage(event.sender.id, {"text": "That's not a place I recognize. Please give me a NYC neighborhood."});
                    } else {
                        //console.log("HERE!");
@@ -451,7 +465,7 @@ app.post('/webhook', function (req, res) {
                        }else {
                            sendMessage(event.sender.id,{"text": "Nice! What is your price range? For example, '1500 to 3000'"});
                        }
-                   }
+                   }*/
                } else if (beds === Number.MAX_VALUE && apartmentType === "Entire Apartment") {
                    //finding bedrooms
                    beds = findBeds(event.message.text);
