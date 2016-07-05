57da6ea29dae017001bea76a249cc87c49eed856
Anni Zhang <annizhang@Annis-MacBook-Pro-II.local>
2016-07-05 15:15:34 -0400

    is redis working???

--- index.js
+++ index.js
@@ -510,7 +510,7 @@ function alertMe(senderId) {
         'beds' : beds });
     console.log("set alert, what is alert:");
     client.get(senderId, function(err, reply) {
-        console.log(reply);
+        console.log(reply.toString());
     });
     if (apartmentType === "Share"){
         message = "Cool! I will alert you when a room listing in " + place +" between " + "$" + minPrice.toString() + " and " + "$" + maxPrice.toString() +  " pops up!";
