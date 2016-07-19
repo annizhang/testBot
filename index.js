763efb72fce2e7f35ace9cf2e2336c751243014f
Anni Zhang <annizhang@Annis-MacBook-Pro-II.local>
2016-07-19 17:53:29 -0400

    wtf is going on

--- index.js
+++ index.js
@@ -723,7 +723,7 @@ app.post('/webhook', function (req, res) {
     var events = req.body.entry[0].messaging;
     for (var i = 0; i < events.length; i++) {
         var event = events[i];
-        var sender = event.recipient_id;
+        var sender = event.sender.id;
         var time = event.timestamp;
         console.log(time);
         if (event.message && event.message.text){
