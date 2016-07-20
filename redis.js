cde0701e75827414002d2d03c980a2527bbaceaf
Anni Zhang <annizhang@Annis-MacBook-Pro-II.local>
2016-07-20 15:35:41 -0400

    redis in separate file

--- redis.js
+++ redis.js
@@ -1,1 +1,14 @@
-var redis = require('redis);
+var redis = require('redis');
+                    
+//connecting to the server
+var client = redis.createClient(process.env.REDIS_URL);
+
+client.on('error', function(err){
+    console.log('Error ' + err);
+});
+
+client.on('connect', function() {
+    console.log("redis connected!");
+});
+
+module.exports = client;
